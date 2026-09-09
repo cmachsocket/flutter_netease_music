76ca58bafd0946eb267ffbd56d9aff5aff7e76d2
cmachsocket <cmach_socket@outlook.com>
2026-09-09 10:01:17 +0800

    add choose to add a songlist

--- lib/SongListPage/LongPressDialog.dart
+++ lib/SongListPage/LongPressDialog.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import '../models/Song.dart';
+import 'package:get/get.dart';
 import '../models/LibrarySummary.dart' show PlaylistSource;
 
 class LongPressDialog extends StatelessWidget {
@@ -18,15 +18,45 @@ class LongPressDialog extends StatelessWidget {
   //占位
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('长按操作'),
-      content: const Text('你长按了这个元素。'),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: const Text('关闭'),
+    if (!Get.isRegistered<LongPressDialogController>()) {
+      Get.put(
+        LongPressDialogController(
+          song: song,
+          index: index,
+          source: source,
+          playlistId: playlistId,
         ),
-      ],
+      );
+    }
+    final controller = Get.find<LongPressDialogController>();
+    return Dialog(
+      child: Obx(() {
+        return controller.isChooseToAdd.value == false
+            ? Column(
+                children: [
+                  ListTile(title: Text('添加到歌单')),
+                  if (source == PlaylistSource.created && playlistId != null)
+                    ListTile(title: Text('从歌单中删除')),
+                  ListTile(title: Text('添加到播放队列')),
+                ],
+              )
+            : Column(children: [ListTile(title: Text('选择歌单'))]);
+      }),
     );
   }
+}
+
+class LongPressDialogController extends GetxController {
+  LongPressDialogController({
+    required this.song,
+    required this.index,
+    required this.source,
+    required this.playlistId,
+  });
+  final Song song;
+  final int index;
+  final PlaylistSource source;
+  final String? playlistId;
+  final RxBool isChooseToAdd = false.obs;
+  void onAddToPlaylist(Song song) {}
 }
